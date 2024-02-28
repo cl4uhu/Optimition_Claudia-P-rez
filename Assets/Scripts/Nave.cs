483d0f043404d27f17bd0d86f3be4c42875a0193
cl4uhu <pjclaudia1@gmail.com>
2024-02-28 11:26:28 +0100

    error solution

--- Assets/Scripts/Nave.cs
+++ Assets/Scripts/Nave.cs
@@ -4,30 +4,24 @@ using UnityEngine;
 
 public class Nave : MonoBehaviour
 {
-    [SerializeField] float playerSpeed = 50f;
+    [SerializeField] float playerSpeed = 30f;
 
     [SerializeField] Transform repositoryPosition;
     [SerializeField] int mineType = 0;
     
-    void Start()
-    {
-        
-    }
-
-    
     void Update()
-    {
+    {        
         float horizontalDisplacement = Input.GetAxis("Horizontal");
 
         Vector3 displacement = new Vector3(horizontalDisplacement, 0f, 0f);
         transform.Translate(displacement * playerSpeed * Time.deltaTime);
 
-        if(Input.GetButtonDown("Fire1"));
+        if(Input.GetButtonDown("Fire1"))
         {
         GameObject repository = PoolManagerNave.Instance.GetPooledObjects(mineType, repositoryPosition.position, repositoryPosition.rotation);
         if(repository != null)
             {
-            repository.SetActive(true);
+                repository.SetActive(true);
             }
         else
             {
